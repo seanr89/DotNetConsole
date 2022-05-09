56b0fd4c5ebc9eae4f1dc4b43b53145945f71331
Sean Rafferty <seanr@randox.com>
2022-05-09 19:43:07 +0100

    updated comments

--- src/MenuApp/MenuOption.cs
+++ src/MenuApp/MenuOption.cs
@@ -1,4 +1,10 @@
 
 namespace MenuApp;
 
+/// <summary>
+/// A breakdown of a single menu event with an action to execute!
+/// </summary>
+/// <param name="Name">The displayed menu name of the Action to run</param>
+/// <param name="Operation">A function/class to execute</param>
+/// <returns>a menu option record</returns>
 public record MenuOption(string Name, Action Operation);
--- src/MenuApp/Program.cs
+++ src/MenuApp/Program.cs
@@ -12,7 +12,10 @@ class Program
 
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Please select from one of the options!");
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleHelpers.cancelHandler);
 
+        // TODO: move to a menu list class to be configured externally
+        // i.e. drive it via DI of a IMenuOption interface
         options = new List<MenuOption>
         {
             new MenuOption("1. Output Sample Message", () => Console.WriteLine("Welcome to the app!")),
@@ -23,45 +26,42 @@ class Program
         _menu = new Menu(options);
 
         // Set the default index of the selected item to be the first
-            int index = 0;
+        int index = 0;
 
-            // Write the menu out
-            _menu.updateMenuWithLocation(options[index], false);
+        // Talk to menu and display menu and default the location
+        _menu.updateMenuWithLocation(options[index], false);
 
-            Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleHelpers.cancelHandler);
+        // Store key info in here
+        ConsoleKeyInfo keyinfo;
+        do
+        {
+            keyinfo = Console.ReadKey();
 
-            // Store key info in here
-            ConsoleKeyInfo keyinfo;
-            do
+            // Handle each key input (down arrow will write the menu again with a different selected item)
+            if (keyinfo.Key == ConsoleKey.DownArrow)
             {
-                keyinfo = Console.ReadKey();
-
-                // Handle each key input (down arrow will write the menu again with a different selected item)
-                if (keyinfo.Key == ConsoleKey.DownArrow)
+                if (index + 1 < options.Count)
                 {
-                    if (index + 1 < options.Count)
-                    {
-                        index++;
-                        _menu.updateMenuWithLocation(options[index], true);
-                    }
-                }
-                if (keyinfo.Key == ConsoleKey.UpArrow)
-                {
-                    if (index - 1 >= 0)
-                    {
-                        index--;
-                        _menu.updateMenuWithLocation(options[index], true);
-                    }
-                }
-                // Handle different action for the option
-                if (keyinfo.Key == ConsoleKey.Enter)
-                {
-                    options[index].Operation.Invoke();
-                    index = 0;
+                    index++;
+                    _menu.updateMenuWithLocation(options[index], true);
                 }
             }
-            while (keyinfo.Key != ConsoleKey.X);
-
-            Console.ReadKey();
+            if (keyinfo.Key == ConsoleKey.UpArrow)
+            {
+                if (index - 1 >= 0)
+                {
+                    index--;
+                    _menu.updateMenuWithLocation(options[index], true);
+                }
+            }
+            // Handle different action for the option
+            if (keyinfo.Key == ConsoleKey.Enter)
+            {
+                options[index].Operation.Invoke();
+                index = 0;
+            }
+        }
+        while (keyinfo.Key != ConsoleKey.X);
+        Console.ReadKey();
     }
 }
