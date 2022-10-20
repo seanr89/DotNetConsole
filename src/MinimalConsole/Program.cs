9f31efd2a2b078d3aa57f7e07d4e2e2f47b380f0
Sean Rafferty <seanr@randox.com>
2022-10-20 21:16:45 +0100

    first example app

---
+++ src/MinimalConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MathCalc class1 = new MathCalc();
+class1.Sum();
+
+
+internal class MathCalc
+{
+    public void Sum() {
+            int a = 5;
+            int b = 6;
+            int Sum = a + b;
+            Console.WriteLine("Sum : {0}", Sum);
+        }
+}
